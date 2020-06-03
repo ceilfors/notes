055c2d3e729b17a70df4069d5d21dce641b10bf3
Wisen Tanasa <wisen@upmo.com>
2020-06-03 06:28:35 +0100

    chore: remove unnecessary version log

--- .script/cron-sync.sh
+++ .script/cron-sync.sh
@@ -1,8 +1,5 @@
 #!/bin/zsh
 export PATH=/Users/wisentanasa/.asdf/shims/:$PATH
-python -v
-node -v
-
 (cd .script; ./sync.sh) # pull things out from Bear
 ./node_modules/.bin/note-link-janitor notes
 (cd .script; ./sync.sh) # sync backlinks back to Bear
