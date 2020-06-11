50d56939f66cd4f5dda372a18203a2c05ae5171e
Wisen Tanasa <wisen@upmo.com>
2020-06-11 10:31:58 +0100

    feat: add backlink help tip

--- notes/.vuepress/enhanceApp.js
+++ notes/.vuepress/enhanceApp.js
@@ -1,15 +1,26 @@
 const createFooterNode = () => {
-  var f = document.createElement('div');
-  f.id = "footer"
-  f.style = "padding: 2.5rem; border-top: 1px solid #eaecef; text-align: center; color: #4e6e8e;"
-  f.innerHTML = '<a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a> | Copyright © 2020-present Wisen Tanasa'
-  return f
+  var n = document.createElement('div');
+  n.id = "footer"
+  n.style = "padding: 2.5rem; border-top: 1px solid #eaecef; text-align: center; color: #4e6e8e;"
+  n.innerHTML = '<a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a> | Copyright © 2020-present Wisen Tanasa'
+  return n
+}
+
+const createBacklinkHelpNode = () => {
+  var n = document.createElement('span');
+  n.id = "backlinks-help"
+  n.classList = "help-tip"
+  n.innerHTML = '<p>These are other notes that link back to this note</p>'
+  return n
 }
 
 const appendCustomDoms = () => {
   if (document.querySelector('main')) {
     if (!document.querySelector("#footer")) {
       document.querySelector('main').appendChild(createFooterNode())
+    }
+    if (!document.querySelector("#backlinks-help") && document.querySelector("#backlinks")) {
+      document.querySelector('#backlinks').appendChild(createBacklinkHelpNode())
     }
   }
 }
