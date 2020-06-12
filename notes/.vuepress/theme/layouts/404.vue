f5a85238369337f5654c4262ac6e5b7d710cdf57
Wisen Tanasa <wisen@upmo.com>
2020-06-12 08:07:35 +0100

    change 404 page

---
+++ notes/.vuepress/theme/layouts/404.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="theme-container">
+    <div class="theme-default-content">
+      <h1>Oops</h1>
+
+      <blockquote>This note is not ready yet</blockquote>
+
+      <a
+        type="button"
+        href="/"
+        @click.prevent="hasHistory()  ? $router.go(-1)  : $router.push('/')"
+      >Go back</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    hasHistory() {
+      return window.history.length > 2;
+    }
+  }
+};
+</script>
