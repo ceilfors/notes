a948f2b7f66a133fd00d281162908e67ec9092bf
Wisen Tanasa <wisen@upmo.com>
2020-10-20 14:18:18 +0100

    feat: add sitemap

--- notes/.vuepress/config.js
+++ notes/.vuepress/config.js
@@ -9,15 +9,17 @@ module.exports = {
       md.use(wikilinks);
     },
   },
+  plugins: {
+    'sitemap': {
+      hostname: 'https://notes.ceilfors.com/'
+    },
+  },
   themeConfig: {
     nav: [
-      // { text: "Radar", link: "/radar/" },
       {
         text: "What's top of mind",
         link: "/§What's_top_of_mind.html",
       },
-      // { text: "Decision Records", link: "/decisions/" },
-      // { text: "Operations", link: "/operations/" },
     ],
   }
 };
